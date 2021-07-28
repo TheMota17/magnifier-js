0d2a247ce70c86d85961f6c05aaae5605874ea31
The Mota <ran777dom@gmail.com>
2021-07-28 15:22:39 +0300

    changes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import Magnifier from "/src/js/Magnifier.js";
 import "./styles.css";
 
 new Magnifier({
-  origImgW: 720,
-  origImgH: 720,
-
   viewImgW: 360,
   viewImgH: 360,
-  
+
   magnifierW: 180,
   magnifierH: 180,
 
   wrapper: "magnifier-wrapper",
   img: "magnifier-img",
-  el: "magnifier",
+  el: "magnifier"
 });
